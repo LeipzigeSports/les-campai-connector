50b396152d9ff8a95c9594ed50b59971ef9b480c
Maximilian Jugl <git@eulenbu.de>
2025-10-03 12:37:43 +0200

    feat: add config for campai

--- les_campai_connector/config.py
+++ les_campai_connector/config.py
@@ -2,6 +2,11 @@ from pydantic import BaseModel, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+class CampaiConfig(BaseModel):
+    api_key: SecretStr
+    base_url: str = "https://api.campai.com"
+
+
 class KeycloakConfig(BaseModel):
     client_id: str
     client_secret: SecretStr
@@ -12,3 +17,4 @@ class KeycloakConfig(BaseModel):
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", env_nested_delimiter="__")
     keycloak: KeycloakConfig
+    campai: CampaiConfig
